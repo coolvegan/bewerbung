5da1bc6fee25769c148f24235e49a0c094f28773
Marco Kittel <marco@planhero.de>
2023-04-07 15:24:02 +0200

    perlin noise

--- sketch.js
+++ sketch.js
@@ -1,33 +1,12 @@
-var w;
-
+var xoff = 0;
 function setup() {
   createCanvas(640, 400);
-  w = new Walker();
 }
 
 function draw() {
   background(51);
-  w.update();
-  w.display();
-}
-
-function Walker() {
-  this.pos = createVector(width / 2, height / 2);
-  this.vel = createVector(0, 0);
-  //this.acc = createVector(0, 0.1);
-  this.acc = p5.Vector.fromAngle(random(0, TWO_PI));
-
-  this.update = function () {
-    var mouse = createVector(mouseX, mouseY);
-    //this.acc = p5.Vector.sub(mouse, this.pos);
-    //this.acc.setMag(0.09);
-    this.acc.rotate(0.1);
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-  };
-
-  this.display = function () {
-    fill(255);
-    ellipse(this.pos.x, this.pos.y, 48, 48);
-  };
+  var x = noise(xoff) * width;
+  xoff += 0.05;
+  fill(255);
+  ellipse(x, 180, 48, 48);
 }
